3ac226f26eb9d70c70e146f7af6cfaa77de37fb3
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-11 21:40:10 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,27 @@
+class Song
+
+  song_count = 0
+  genre_count = 0
+  artists = []
+  genres = []
+
+  def initialize
+    @song_count += 1
+
+  end
+
+  def count
+    @song_count
+  end
+
+  def artists
+    artists
+  end
+
+  def genres
+    genres
+  end
+
+
+
+end
