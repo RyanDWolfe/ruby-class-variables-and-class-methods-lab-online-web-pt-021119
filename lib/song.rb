7d201cd4efa146243b60d146fb2964e79693d732
Ryan Wolfe <ryan@studiohopfitness.com>
2019-03-11 21:57:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,11 +27,17 @@ class Song
     @@genres.uniq
   end
 
-  def artist_count
-
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count(artist)
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+    end
   end
 
-  def genre_count
+  def self.genre_count
 
   end
 
